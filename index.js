fa899f09a8b2be503634564f667eb56a14659596
Hrid <Hrid1402@gmail.com>
2024-03-08 12:20:45 -0500

    Finished

--- index.js
+++ index.js
@@ -14,16 +14,55 @@ const passwordError = document.querySelector('#passwordError');
 const C_password = document.querySelector('#C_password');
 const C_passwordError = document.querySelector('#C_passwordError');
 
+const errorColorStyle = "2px solid red"
+const goodColorStyle = "2px solid rgba(255, 255, 255, 0.661)"
+function errorColor(element){
+    if (Array.isArray(element)){
+        element.forEach(i => {
+            i.style.border = errorColorStyle;
+        });
+    }
+    else{
+        element.style.border = errorColorStyle;
+    }
+    
+}
+
+function goodColor(element){
+    if (Array.isArray(element)){
+        element.forEach(i => {
+            i.style.border = goodColorStyle;
+        });
+    }
+    else{
+        element.style.border = goodColorStyle;
+    }
+    
+}
+
+goodColor([email, country , zipCode, password, C_password]);
+
+
+
+
 email.addEventListener('keyup',()=>{
     if(email.validity.patternMismatch){
         emailError.textContent = "Incorrect Format";
         emailError.style.opacity = "100%";
+
+        errorColor(email);
+
+
     }else if(email.validity.valueMissing){
         emailError.textContent = "Empty space";
         emailError.style.opacity = "100%";
+
+        errorColor(email);
     }
     else{
         emailError.style.opacity = "0%";
+        
+        goodColor(email);
     }
 });
 
@@ -31,12 +70,16 @@ country.addEventListener('keyup',()=>{
     if(country.validity.patternMismatch){
         contryError.textContent = "Dont put numbers";
         contryError.style.opacity = "100%";
+        errorColor(country);
+
     }else if(country.validity.valueMissing){
         contryError.textContent = "Empty space";
         contryError.style.opacity = "100%";
+        errorColor(country);
     }
     else{
         contryError.style.opacity = "0%";
+        goodColor(country);
     }
 });
 
@@ -45,12 +88,15 @@ zipCode.addEventListener('keyup',()=>{
         console.log("missmatch")
         zipCodeError.textContent = "Just numbers are allowed";
         zipCodeError.style.opacity = "100%";
+        errorColor(zipCode);
     }else if(zipCode.validity.valueMissing){
         zipCodeError.textContent = "Empty space";
         zipCodeError.style.opacity = "100%";
+        errorColor(zipCode);
     }
     else{
         zipCodeError.style.opacity = "0%";
+        goodColor(zipCode);
     }
 });
 
@@ -58,8 +104,16 @@ password.addEventListener('keyup',()=>{
     if(password.validity.tooShort){
         passwordError.textContent = "Min 8 characters";
         passwordError.style.opacity = "100%";
+        errorColor(password);
+    }else if(password.value != C_password.value){
+        C_passwordError.textContent = "passwords don't match";
+        C_passwordError.style.opacity = "100%";
+        passwordError.style.opacity = "0%";
+        goodColor(password);
+        errorColor(C_password);
     }else{
         passwordError.style.opacity = "0%";
+        goodColor(password);
     }
 });
 
@@ -67,10 +121,13 @@ C_password.addEventListener('keyup',()=>{
     if(C_password.validity.tooShort){
         C_passwordError.textContent = "Min 8 characters";
         C_passwordError.style.opacity = "100%";
-    }else if(password.value == C_password.value){
+        errorColor(C_password);
+    }else if(password.value != C_password.value){
         C_passwordError.textContent = "passwords don't match";
         C_passwordError.style.opacity = "100%";
-    }else {
+        errorColor(C_password);
+    }else if(password.value == C_password.value){
         C_passwordError.style.opacity = "0%";
+        goodColor(C_password);
     }
 });
